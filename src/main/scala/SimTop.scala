24379987aafe625e114f366ff484ec00cff871c1
Li Shi <shili2048@gmail.com>
2022-08-08 20:11:36 -0400

    Zhoushan-on-CONNECT

--- src/main/scala/SimTop.scala
+++ src/main/scala/SimTop.scala
@@ -18,6 +18,7 @@ package zhoushan
 import chisel3._
 import chisel3.util._
 import difftest._
+import connect_axi._
 
 class SimTop extends Module {
   val io = IO(new Bundle {
@@ -34,10 +35,35 @@ class SimTop extends Module {
 
   val core2axi = Module(new CoreBus2Axi(new AxiIO))
   core2axi.in <> crossbar.io.out
-  core2axi.out <> io.memAXI_0
+
+  /* Original topology
+   *   +------+            +---------------------+
+   *   | Core | <--AXI4--> | DRAM (SW-simulated) |
+   *   +------+            +---------------------+
+   * 
+   * New topology under test
+   *   +------+            +---------------------+
+   *   | Core | <--AXI4--> | M0               S0 | <--AXI4-+
+   *   +------+            |                     |         |
+   *                       |       CONNECT    M1 | <-------+
+   *                       |                     |            +---------------------+
+   *                       |                  S1 | <--AXI4--> | DRAM (SW-simulated) |
+   *                       +---------------------+            +---------------------+
+   */
+
+  val network_configs = GetImplicitNetworkConfigs("AXI4")
+  val network = Module(new NetworkAXI4Wrapper()(network_configs))
+  network.io.clock_noc := clock
+  network.io.master(0) <> core2axi.out
+  network.io.slave(0) <> network.io.master(1)
+  network.io.slave(1) <> io.memAXI_0
+
+  network.io.master(0).aw.bits.asInstanceOf[AXI4ChannelA].user := Cat(core2axi.io.out.aw.bits.user, 2.U(2.W))
+  network.io.master(0).ar.bits.asInstanceOf[AXI4ChannelA].user := Cat(core2axi.io.out.ar.bits.user, 2.U(2.W))
+  network.io.master(1).aw.bits.asInstanceOf[AXI4ChannelA].user := Cat(core2axi.io.out.aw.bits.user, 3.U(2.W))
+  network.io.master(1).ar.bits.asInstanceOf[AXI4ChannelA].user := Cat(core2axi.io.out.ar.bits.user, 3.U(2.W))
 
   io.uart.out.valid := false.B
   io.uart.out.ch := 0.U
   io.uart.in.valid := false.B
-
 }
